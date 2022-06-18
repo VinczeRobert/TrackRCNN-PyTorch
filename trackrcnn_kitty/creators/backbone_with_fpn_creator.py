#14:Add the possibility to use batch normalization that is not freezed + improved loading of weights + bux fixing.

trackrcnn_kitty/creators/backbone_with_fpn_creator.py
@@ -1,4 +1,5 @@
 import torchvision
+from torch.nn import BatchNorm2d
 from torchvision.models.detection.backbone_utils import BackboneWithFPN
 from torchvision.ops import misc as misc_nn_ops
 from torchvision.ops.feature_pyramid_network import LastLevelMaxPool
@@ -6,15 +7,25 @@ from torchvision.ops.feature_pyramid_network import LastLevelMaxPool
 
 class BackboneWithFPNCreator:
 
-    def __init__(self, use_resnet_101, trainable_backbone_layers, pretrain_backbone):
-        # We create the backbone: we'll use a pretrained Resnet101
-        # that has been trained on the COCO dataset
-        if use_resnet_101:
-            backbone = torchvision.models.resnet101(pretrained=pretrain_backbone,
-                                                    norm_layer=misc_nn_ops.FrozenBatchNorm2d)
+    def __init__(self, **kwargs):
+        use_resnet101 = kwargs.get("use_resnet101", True)
+        trainable_backbone_layers = kwargs.get("trainable_backbone_layers", True)
+        pretrained_backbone = kwargs.get("pretrained_backbone", True)
+        freeze_batchnorm = kwargs.get("freeze_batchnorm", False)
+
+        # Using a freezed batch norm layer does not only not train those layers, but
+        # it does not even add the weights to the list of the model's weights
+        if freeze_batchnorm:
+            norm_layer = misc_nn_ops.FrozenBatchNorm2d
         else:
-            backbone = torchvision.models.resnet50(pretrained=pretrain_backbone,
-                                                   norm_layer=misc_nn_ops.FrozenBatchNorm2d)
+            norm_layer = BatchNorm2d
+
+        if use_resnet101:
+            backbone = torchvision.models.resnet101(pretrained=pretrained_backbone,
+                                                    norm_layer=norm_layer)
+        else:
+            backbone = torchvision.models.resnet50(pretrained=pretrained_backbone,
+                                                   norm_layer=norm_layer)
 
         self.backbone_with_fpn = self.resnet_fpn_extractor(backbone, trainable_backbone_layers)
 

trackrcnn_kitty/models/mask_rcnn.py
@@ -1,7 +1,9 @@
+import os.path
 from collections import OrderedDict
 
 import torch.jit
 from torch import Tensor
+from torch.hub import load_state_dict_from_url
 from torchvision.models.detection import MaskRCNN
 from torchvision.models.detection._utils import overwrite_eps
 from torchvision.models.detection.anchor_utils import AnchorGenerator
@@ -17,44 +19,38 @@ model_urls = {
 }
 
 COCO_DATASET_CLASSES = 91
-RPN_BATCH_SIZE_PER_IMG_DEFAULT = 256
 
 
 class CustomMaskRCNN(MaskRCNN):
     def __init__(self,
                  num_classes,
                  backbone,
-                 pretrain_only_backbone,
+                 pretrained_backbone,
                  maskrcnn_params,
                  fixed_size=(1024, 309),
                  **kwargs):
         # In some cases we create a new anchor generator to use smaller anchors (normally,
         # when the images and objects are too small)
         rpn_anchor_generator = None
-        rpn_batch_size_per_image = None
         if maskrcnn_params is not None and isinstance(maskrcnn_params, dict):
             if "anchor_sizes" in maskrcnn_params and "aspect_ratios" in maskrcnn_params:
                 anchor_sizes = tuple([(size,) for size in maskrcnn_params["anchor_sizes"]])
                 aspect_ratios = (tuple(maskrcnn_params["aspect_ratios"]),) * len(anchor_sizes)
                 rpn_anchor_generator = AnchorGenerator(anchor_sizes, aspect_ratios)
-                rpn_batch_size_per_image = maskrcnn_params.get("rpn_batch_size_per_image",
-                                                               RPN_BATCH_SIZE_PER_IMG_DEFAULT)
 
+                maskrcnn_params.pop("anchor_sizes", None)
+                maskrcnn_params.pop("aspect_ratios", None)
+
+        kwargs.update(maskrcnn_params)
         # The number of classes of the COCO dataset that the backbone is pretrained one is 91
-        # Also we want to use 32 ROIs per image because the images don't have many objects
         super(CustomMaskRCNN, self).__init__(backbone, COCO_DATASET_CLASSES, rpn_anchor_generator=rpn_anchor_generator,
-                                             rpn_batch_size_per_image=rpn_batch_size_per_image,
-                                             rpn_pre_nms_top_n_train=1000,
-                                             rpn_pre_nms_top_n_test=500,
-                                             rpn_post_nms_top_n_train=1000,
-                                             rpn_post_nms_top_n_test=500,
                                              **kwargs)
         # Override the transform class to perform resize with fixed size the way it is described in the paper
         image_mean = [0.485, 0.456, 0.406]
         image_std = [0.229, 0.224, 0.225]
         min_size = 800
         max_size = 1333
-        self.transform = GeneralizedRCNNTransform(min_size, max_size, image_mean, image_std, fixed_size=fixed_size)
+        self.transform = GeneralizedRCNNTransform(min_size, max_size, image_mean, image_std)
 
         # Override the RoI heads to have access to custom forward method
         self.roi_heads = RoIHeadsCustom(backbone.out_channels,
@@ -64,14 +60,10 @@ class CustomMaskRCNN(MaskRCNN):
                                         self.roi_heads.mask_predictor)
 
         self.finetune(num_classes)
-        if pretrain_only_backbone is False:
-            self.load_weights_pretrained_on_coco()
 
     def forward(self, images, targets=None):
         if self.training and targets is None:
             raise ValueError("In training mode, targets should be passed")
-
-        stacked_boxes = validate_and_build_stacked_boxes(targets, self.training)
 
         original_image_sizes = []
         for img in images:
@@ -101,14 +93,34 @@ class CustomMaskRCNN(MaskRCNN):
 
         return detections
 
-    # TODO: Fix this method or remove it
-    def load_weights_pretrained_on_coco(self):
-        # state_dict = load_state_dict_from_url(model_urls["maskrcnn_resnet50_fpn_coco"], progress=True)
-        # self.load_state_dict(state_dict)
-        # overwrite_eps(self, 0.0)
-        state_dict = torch.load("mask_rcnn_coco.pth")
-        self.load_state_dict(state_dict, strict=False)
-        overwrite_eps(self, 0.0)
+    def __preprocess_coco_weights(self, state_dict):
+        pass
+
+    def load_weights(self, weights_path, preprocess_weights=False, use_resnet101=False):
+        try:
+            if os.path.exists(weights_path):
+                state_dict = torch.load(weights_path)
+
+                if preprocess_weights:
+                    # This project includes some unofficial weights that are obtained
+                    # from pretraining MaskRCNN on Coco, but they require some changes
+                    # in order to load them in Pytorch
+                    state_dict = self.__preprocess_coco_weights(state_dict)
+                self.load_state_dict(state_dict["model_state"])
+
+            else:
+                # If we don't have a valid weights path we are going to try
+                # to download one from the internet
+                # Unfortunately on PyTorch pretrained weights are available only for ResNet50
+                if use_resnet101 is False:
+                    state_dict = load_state_dict_from_url(model_urls["maskrcnn_resnet50_fpn_coco"])
+                    self.load_state_dict(state_dict)
+
+            overwrite_eps(self, 0.0)
+        except RuntimeError as e:
+            print("There is no valid set of weights that can be loaded."
+                  "Training will start with newly initialized weights!")
+            print(e)
 
     def finetune(self, num_classes):
         # get number of input features for the classifier

trackrcnn_kitty/train_engine.py
@@ -8,6 +8,7 @@ from trackrcnn_kitty.creators.backbone_with_fpn_creator import BackboneWithFPNCr
 from trackrcnn_kitty.creators.data_loader_creator import get_data_loaders
 from trackrcnn_kitty.datasets.dataset_factory import get_dataset
 from trackrcnn_kitty.datasets.transforms import get_transforms
+from trackrcnn_kitty.models.mask_rcnn import CustomMaskRCNN
 from trackrcnn_kitty.models.track_rcnn import TrackRCNN
 
 from trackrcnn_kitty.utils import write_detection_to_file, write_gt_to_file
@@ -23,26 +24,40 @@ class TrainEngine:
         self.config = config
 
         train = True if self.config.task in ["train", "train+val"] else False
-
         transforms = get_transforms(self.config.transforms_list, train)
-        self.dataset = get_dataset(self.config.dataset, self.config.dataset_path, transforms, train)
-        self.data_loaders = get_data_loaders(self.dataset, self.config.dataset, self.config.task,
-                                             self.config.train_batch_size, self.config.test_batch_size,
-                                             self.config.transforms_list)
+
+        self.dataset = get_dataset(config, transforms, train)
+        self.data_loaders = get_data_loaders(self.dataset, self.config)
 
         backbone = BackboneWithFPNCreator(trainable_backbone_layers=self.config.trainable_backbone_layers,
-                                          use_resnet_101=self.config.use_resnet_101,
-                                          pretrain_backbone=self.config.pretrain_only_backbone).get_instance()
+                                          use_resnet101=self.config.use_resnet101,
+                                          pretrained_backbone=self.config.pretrained_backbone,
+                                          freeze_batchnorm=self.config.freeze_batchnorm).get_instance()
 
-        self.model = TrackRCNN(num_classes=self.dataset.num_classes,
-                               backbone=backbone,
-                               pretrain_only_backbone=self.config.pretrain_only_backbone,
-                               maskrcnn_params=self.config.maskrcnn_params)
+        if self.config.add_associations:
+            self.model = TrackRCNN(num_classes=self.dataset.num_classes,
+                                   backbone=backbone,
+                                   pretrained_backbone=self.config.pretrained_backbone,
+                                   maskrcnn_params=self.config.maskrcnn_params)
+        else:
+            self.model = CustomMaskRCNN(num_classes=self.dataset.num_classes,
+                                        backbone=backbone,
+                                        pretrained_backbone=self.config.pretrained_backbone,
+                                        maskrcnn_params=self.config.maskrcnn_params)
+
+            # If the backbone was no pretrained weights, we are going to try to use
+            # pretrained weights for the whole model
+            if self.config.pretrained_backbone is False:
+                self.model.load_weights(self.config.weights_path, self.config.preprocess_weights,
+                                        self.config.use_resnet101)
+
         self.model.to(self.device)
 
     def training(self):
         params = [p for p in self.model.parameters() if p.requires_grad]
-        optimizer = torch.optim.SGD(params, lr=self.config.learning_rate)
+        optimizer = torch.optim.SGD(params, lr=self.config.learning_rate,
+                                    weight_decay=self.config.weight_decay,
+                                    momentum=self.config.momentum)
 
         for epoch in range(self.config.num_epochs):
             # train for one epoch, printing every 10 iterations

trackrcnn_kitty/train_on_coco.py
@@ -25,8 +25,8 @@ if __name__ == '__main__':
     val_set = MainCOCODataset(dataset_val, config, augment=True)
     val_generator = torch.utils.data.DataLoader(val_set, batch_size=1, shuffle=True, num_workers=4, collate_fn=collate_fn)
 
-    backbone = BackboneWithFPNCreator(use_resnet_101=True,
-                                      pretrain_backbone=False,
+    backbone = BackboneWithFPNCreator(use_resnet101=True,
+                                      pretrained_backbone=False,
                                       trainable_backbone_layers=5).get_instance()
 
     maskrcnn_params = {
@@ -37,7 +37,7 @@ if __name__ == '__main__':
 
     model = CustomMaskRCNN(num_classes=91,
                            backbone=backbone,
-                           pretrain_only_backbone=False,
+                           pretrained_backbone=False,
                            maskrcnn_params=maskrcnn_params,
                            fixed_size=(1024, 309))
     model.to(device)