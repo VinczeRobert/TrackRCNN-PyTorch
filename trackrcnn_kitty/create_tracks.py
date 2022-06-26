e4b152d93f75bc93a3d6129d076c36cb68df9a8b
rvincze <robert.vincze@anaplan.com>
2022-06-26 12:35:28 +0300

    #23: Improve tracking algorithm.

--- trackrcnn_kitty/create_tracks.py
+++ trackrcnn_kitty/create_tracks.py
@@ -2,15 +2,18 @@
 For now this script is only going to loop through one KITTI sequence
 and link the ground truths in time, without any training happening.
 """
-import torch
-import numpy as np
 import munkres
+import numpy as np
+import torch
 from sklearn.metrics import jaccard_score
 
+import trackrcnn_kitty.datasets.transforms as T
 from references.pytorch_detection.utils import collate_fn
 from trackrcnn_kitty.adnotate import adnotate
 from trackrcnn_kitty.datasets.kitti_seg_track_dataset import KITTISegTrackDataset
-from trackrcnn_kitty.datasets.transforms import get_transforms
+
+MATCHING_THRESHOLD_CAR = 0.25
+MATCHING_THRESHOLD_PEDESTRIAN = 0.15
 
 
 def compute_overlaps_masks(masks1, masks2):
@@ -31,10 +34,22 @@ def compute_overlaps_masks(masks1, masks2):
     return scores
 
 
+def get_transforms(transforms_list, train):
+    transforms = [T.ToTensor()]
+
+    if train:
+        if "flip" in transforms_list:
+            transforms.append(T.RandomHorizontalFlip(0.5))
+        if "gamma" in transforms:
+            transforms.append(T.GammaCorrection((-0.05, 0.05)))
+
+    return T.Compose(transforms)
+
+
 def main():
     root_path = "/Users/robert.vincze/Downloads/KITTITrackSegValDataset/"
     transforms = get_transforms([], False)
-    reduced_dataset = KITTISegTrackDataset(root_path, transforms, False, "0002")
+    reduced_dataset = KITTISegTrackDataset(root_path, transforms, False, "0006")
     batch_size = 8
     data_loader = torch.utils.data.DataLoader(
         reduced_dataset,
@@ -59,14 +74,14 @@ def main():
             # The very first image can't be compared to any previous frame
             is_first_batch = False
             colors = []
-            object_ids = []
+            obj_ids = []
             for i in range(len(targets[0]["masks"])):
                 random_color = np.random.choice(range(256), size=3)
                 colors.append(random_color)
-                object_ids.append(obj_id_count)
+                obj_ids.append(obj_id_count)
                 association_dict[obj_id_count] = (random_color, obj_id_count)
                 obj_id_count = obj_id_count + 1
-            adnotate(targets[0]["image_path"], targets[0]["masks"], object_ids, colors)
+            adnotate(targets[0]["image_path"], targets[0]["masks"], obj_ids, colors)
         else:
             # Insert the last element of the previous batch at the beginning of the list
             targets = list(targets)
@@ -82,42 +97,56 @@ def main():
             mask_overlaps = mask_overlaps * (-1)
 
             # Get difference between rows and columns
-            orig_row_numbers = mask_overlaps.shape[0]
             orig_columns_numbers = mask_overlaps.shape[1]
             row_column_diff = mask_overlaps.shape[0] - mask_overlaps.shape[1]
             extra_columns = []
-            extra_rows = []
             if row_column_diff > 0:
                 # There are more rows than columns, columns need to be padded
                 mask_overlaps = np.pad(mask_overlaps, [(0, 0), (0, row_column_diff)], mode='constant')
-                extra_rows = [k for k in range(orig_columns_numbers, orig_columns_numbers + row_column_diff)]
+                extra_columns = [k for k in range(orig_columns_numbers, orig_columns_numbers + row_column_diff)]
             elif row_column_diff < 0:
                 # There are more columns than rows, rows need to be added
                 mask_overlaps = np.pad(mask_overlaps, [(0, abs(row_column_diff)), (0, 0)], mode='constant')
-                extra_columns = [k for k in range(orig_row_numbers, orig_row_numbers + abs(row_column_diff))]
 
             # this method works directly on the inputs
             # it returns a list of tuples of dim 2
-            indexes = hungarian_algorithm.compute(mask_overlaps)
+            pairs = hungarian_algorithm.compute(mask_overlaps.copy())
 
-            # extra columns need to be eliminated from indexes because they will be added as new objects
-            # extra rows also need to be eliminated from indexes because they are objects from the
-            # previous frame which don't appear in the current one
-            indexes = list(filter(lambda x: x[1] not in extra_rows and x[0] not in extra_columns, indexes))
+            mask_overlaps = mask_overlaps * (-1)
+            non_matching_pairs = []
+            # eliminate pairs which have an IoU smaller than MATCHING_THRESHOLD
+            for i in range(len(pairs) - 1, -1, -1):
+                try:
+                    class_jm1 = targets[j]["labels"][pairs[i][0]].item()
+                except IndexError:
+                    class_jm1 = -1
+                try:
+                    class_jm = targets[j]["labels"][pairs[i][1]].item()
+                except IndexError:
+                    class_jm = -1
+
+                # eliminate matches with overlapping less than a threshold or where the classes don't correspond
+                threshold = MATCHING_THRESHOLD_CAR if class_jm == 1 else MATCHING_THRESHOLD_PEDESTRIAN
+                if (mask_overlaps[pairs[i][0]][pairs[i][1]] < threshold) or \
+                        (class_jm != class_jm1 and class_jm != -1 and class_jm1 != -1):
+                    non_matching_pairs.append(pairs[i])
+                    del pairs[i]
+
             next_association_dict = dict()
-
             obj_ids = [0] * mask_overlaps.shape[1]
             colors = [0] * mask_overlaps.shape[1]
 
-            # Add anything in extra columns as a new detection
-            for ex in extra_columns:
-                obj_ids[ex] = obj_id_count + 1
-                random_color = np.random.choice(range(256), size=3)
-                colors[ex] = random_color
-                obj_id_count = obj_id_count + 1
-                next_association_dict[ex] = (random_color, obj_id_count)
+            # Loop through the non matching pairs and if the column is not one
+            # that was artificially added for the Jaccard score, then add it
+            for idx in non_matching_pairs:
+                if idx[1] not in extra_columns:
+                    obj_ids[idx[1]] = obj_id_count
+                    random_color = np.random.choice(range(256), size=3)
+                    colors[idx[1]] = random_color
+                    next_association_dict[idx[1]] = (random_color, obj_id_count)
+                    obj_id_count = obj_id_count + 1
 
-            for index in indexes:
+            for index in pairs:
                 obj_id = association_dict[index[0]][1]
                 color = association_dict[index[0]][0]
                 obj_ids[index[1]] = obj_id
@@ -125,9 +154,10 @@ def main():
                 next_association_dict[index[1]] = (color, obj_id)
 
             # delete detections from previous frame that are not present here
-            if len(extra_rows) > 0:
-                obj_ids = obj_ids[:-len(extra_rows)]
-                colors = colors[:-len(extra_rows)]
+            # The new fake columns would always be at the end of this list
+            if len(extra_columns) > 0:
+                obj_ids = obj_ids[:-len(extra_columns)]
+                colors = colors[:-len(extra_columns)]
 
             del association_dict
             association_dict = next_association_dict
